bf575c2561509944db0295cd324c74169b129a9b
Warrzcoding <ilustrisimowarren94@gmail.com>
2025-08-31 00:39:11 +0800

    s

--- resources/views/pre_signup.blade.php
+++ resources/views/pre_signup.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Pre-Sign Up - Office Performance Evaluation System</title>
+    <title>Pre-Sign Up - Instructors Performance Evaluation System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
