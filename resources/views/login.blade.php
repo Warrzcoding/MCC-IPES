6482355d3fe633256e9df0664e30d19e849224e2
Warrzcoding <ilustrisimowarren94@gmail.com>
2025-09-30 19:09:45 +0800

    cxc

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -921,7 +921,7 @@
                             <select class="form-select" id="user_type" name="user_type" onchange="handleUserTypeChange()" required>
                                 <option value="">Choose your role...</option>
                                 <option value="student">Students</option>
-                                <option value="admin">Administrator</option>
+                                <option value="admin">Administratoccr</option>
                                 <!--<option value="staff">CSVLoader</option>-->
                             </select>
                         </div>
