dc0b1cabac2fb74d98b1b09d5784e7bccb3860a5
Warrzcoding <ilustrisimowarren94@gmail.com>
2025-10-27 21:10:44 +0800

    c

--- resources/views/reset_password.blade.php
+++ resources/views/reset_password.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Reset Password - Office Performance Evaluation System</title>
     <link rel="icon" type="image/png" href="{{ asset('images/mccicon.jpg') }}">
