f79da01e284ef2c08c2724e032fa067cd67de1c5
Warrzcoding <ilustrisimowarren94@gmail.com>
2025-10-27 21:21:40 +0800

    ss

--- resources/views/s_admin/superadminhome.blade.php
+++ resources/views/s_admin/superadminhome.blade.php
@@ -6,7 +6,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Super Admin Dashboard - MCCIPES</title>
     <link rel="icon" type="image/png" href="{{ asset('images/mccicon.jpg') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
