58de9ed2b60dbc71b6581d9e93b83cdb12d104b3
Warrzcoding <ilustrisimowarren94@gmail.com>
2025-11-13 20:11:52 +0800

    ddf

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -239,7 +239,7 @@
                             @enderror
                         </div>
                         <div class="col-md-6 mb-3">
-                            <label class="form-label">Username <span class="text-danger">*</span></label>
+                            <label class="form-label">Usernamed <span class="text-danger">*</span></label>
                             <input type="text" class="form-control @error('username') is-invalid @enderror" 
                                    name="username" value="{{ old('username', Auth::user()->username) }}" required>
                             @error('username')
