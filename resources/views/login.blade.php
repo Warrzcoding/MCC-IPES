315a6497f70afbc41788050ff6bf585688ccfda5
Warrzcoding <ilustrisimowarren94@gmail.com>
2025-11-18 07:14:01 +0800

    updated login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1536,7 +1536,7 @@
                     @endif
 
                     <button type="submit" name="login" class="btn btn-primary">
-                        <i class="fas fa-sign-in-alt"></i> Login as CSVs
+                        <i class="fas fa-sign-in-alt"></i> Login as CSV
                     </button>
                     <button type="button" class="btn-back-icon" onclick="resetForm()" aria-label="Back">
                         <i class="fas fa-arrow-left"></i>
