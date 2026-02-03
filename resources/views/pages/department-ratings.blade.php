99e7103d4b19646a2c46f561080c1520412d62e6
Warrzcoding <ilustrisimowarren94@gmail.com>
2026-02-03 15:02:04 +0800

    as

--- resources/views/pages/department-ratings.blade.php
+++ resources/views/pages/department-ratings.blade.php
@@ -1268,7 +1268,7 @@ function getAdjectivalRating($rating) {
                 </div>
 
                 <!-- Signature Section -->
-                <div style='margin-top:1em;margin-bottom:0em;text-align:left; font-size:10pt; line-height:1.3;'>
+                <div style='page-break-before: avoid; margin-top:3em; margin-bottom:0em; text-align:left; font-size:10pt; line-height:1.3;'>
                     <div style='margin-bottom:1em;'>
                         Prepared by:
                     </div>
