dfd865017e7f812e80e4e70a62be51eb3db2e1ce
Warrzcoding <ilustrisimowarren94@gmail.com>
2026-02-25 16:39:12 +0800

    sd

--- resources/views/pages/overall-ratings.blade.php
+++ resources/views/pages/overall-ratings.blade.php
@@ -761,7 +761,7 @@ function printOverallReport(staffData, type) {
                 
                <p style="margin-bottom: 1em;">Greetings of Peace!</p>
      
-                <p style="margin-bottom: 0.7em;">I am writing to formally endorse the Results Summary of Performance Evaluation Results of all MCC ${title} for Academic Year {{ $currentAcademicYear?->year }} - {{ $currentAcademicYear?->semester == 1 ? 'First' : ($currentAcademicYear?->semester == 2 ? 'Second' : $currentAcademicYear?->semester) }} Semester.</p>
+                <p style="margin-bottom: 0.7em;">I am writing to formally endorse the Results of the Performance Evaluation of all MCC ${title} for Academic Year {{ $currentAcademicYear?->year }} - {{ $currentAcademicYear?->semester == 1 ? 'First' : ($currentAcademicYear?->semester == 2 ? 'Second' : $currentAcademicYear?->semester) }} Semester.</p>
                 
                 <p style="margin-bottom: 0.7em;">Please be advised that the following ${title.toLowerCase()} were evaluated by all MCC Students.</p>
             </div>
