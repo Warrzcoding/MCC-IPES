d201ff4c0211c5ee2794e0ea2327a2547f99316f
Warrzcoding <ilustrisimowarren94@gmail.com>
2026-05-08 16:03:25 +0800

    taste 3 overall

--- resources/views/pages/overall-ratings.blade.php
+++ resources/views/pages/overall-ratings.blade.php
@@ -808,7 +808,7 @@ function printOverallReport(staffData, type) {
             </div>
 
             <!-- Signature Section update margintop-->
-            <div style='margin-top:6em;margin-bottom:0em;text-align:left; font-size:10pt; line-height:1.3;'>
+            <div style='margin-top:8em;margin-bottom:0em;text-align:left; font-size:10pt; line-height:1.3;'>
                 <div style='margin-bottom:1em;'>
                     Prepared by:
                 </div>
