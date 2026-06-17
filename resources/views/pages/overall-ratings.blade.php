00ccb77d05ca04aeea387be8e0cee360b81e33cb
Warrzcoding <ilustrisimowarren94@gmail.com>
2026-06-17 12:46:00 +0800

    ccc

--- resources/views/pages/overall-ratings.blade.php
+++ resources/views/pages/overall-ratings.blade.php
@@ -809,7 +809,7 @@ function printOverallReport(staffData, type) {
 
             <!-- Signature Section update margintop-->
             <div style='margin-top:3em;margin-bottom:0em;text-align:left; font-size:9pt; line-height:1.3;'>
-            <br><br>
+            <br>
                 <div style='margin-bottom:1em;'>
                     Prepared by:
                 </div>
